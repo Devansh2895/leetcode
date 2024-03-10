c746f5a5c2416d5bb8011e21d829a63ce52ee7bc
Devansh Bansal <devansh2895@gmail.com>
2024-03-10 16:41:15 +0530

    Update 27. Remove Element.cpp

--- Top Interview 150/27. Remove Element.cpp
+++ Top Interview 150/27. Remove Element.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
+    int removeElement(vector<int>& nums, int val) {
         int size = nums.size();
-        int k = 1;
-        for (int i=0, j=1; i<size-1 && j<size; ++i) {
-            if (nums[i] < nums[i+1]) {
-                nums[j] = nums[i+1];
-                ++j;
-                ++k;
+        int j = size-1;
+        int count = 0;
+        for (int i=0; i<=j; ++i) {
+            while (j>=0 && nums[j]==val) {
+                nums[j]=-1;
+                ++count;
+                --j;
+            }
+            if (nums[i] == val) {
+                ++count;
+                nums[i] = nums[j];
+                nums[j] = -1;
+                --j;
             }
         }
-        return k;
+        return size-count;
     }
 };
