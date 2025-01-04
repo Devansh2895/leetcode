1837cffa7f1414f8a8f65b5be05f02f3e8c28ba1
Devansh Bansal <devansh2895@gmail.com>
2025-01-04 21:28:33 +0530

    Update 994. Rotting Oranges.cpp

--- Other/994. Rotting Oranges.cpp
+++ Other/994. Rotting Oranges.cpp
@@ -11,18 +11,17 @@ public:
         int num1s = 0;
         vector<int> nx = {0,0,1,-1};
         vector<int> ny = {1,-1,0,0};
-        vector<vector<int>> visited(m, vector<int>(n));
         queue<vector<int>> q;
         for (int i=0; i<m; i++) {
             for (int j=0; j<n; j++) {
                 if (grid[i][j] == 1)
-                    num1s++;
+                    ++num1s;
                 else if (grid[i][j] == 2)
                     q.push({i,j});
             }
         }
         if (num1s == 0) return 0;
-        int mnt=0;
+        int mnt=-1;
         while (!q.empty()) {
             int s = q.size();
             while(s--) {
@@ -30,29 +29,20 @@ public:
                 int i = idx[0];
                 int j = idx[1];
                 q.pop();
-                visited[i][j] = 1;
-                int ai;
-                int aj;
-                for (int a=0; a<4; a++) {
-                    ai = i+nx[a];
-                    aj = j+ny[a];
+                grid[i][j] = 2;
+                for (int a=0; a<4; ++a) {
+                    int ai = i+nx[a];
+                    int aj = j+ny[a];
 
-                    if ((inGrid(ai, aj, m, n) && grid[ai][aj] == 1) && visited[ai][aj]==0) {
-                        visited[ai][aj] = 1;
+                    if (inGrid(ai, aj, m, n) && grid[ai][aj] == 1) {
                         grid[ai][aj] = 2;
+                        --num1s;
                         q.push({ai,aj});
                     }
                 }
             }
-            cout<<endl;
             mnt++;
         }
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
-                if (grid[i][j] == 1)
-                    return -1;
-            }
-        }
-        return mnt-1;
+        return num1s==0 ? mnt : -1;
     }
 };
